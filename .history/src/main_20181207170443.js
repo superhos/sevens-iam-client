64911b6a37df43efd95a1349f26c1d8099324fc9
Chen, Steven H <Steven.H.Chen@ctm.com.mo>
2018-12-07 18:02:11 +0800

    1. init

--- .history/src/main_20181207170443.js
+++ .history/src/main_20181207170443.js
@@ -1,14 +1,10 @@
 import Vue from 'vue'
-import Antd from 'ant-design-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
-import 'ant-design-vue/dist/antd.css'
-
 Vue.config.productionTip = false
-Vue.use(Antd)
 
 new Vue({
   router,
